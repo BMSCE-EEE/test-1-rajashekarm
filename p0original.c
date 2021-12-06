62324071b7d127285347b51d55ceaf1f06877295
Raj Shekar <94944999+rajashekarm@users.noreply.github.com>
2021-12-06 03:29:47 +0000

    p4

--- p0original.c
+++ p0original.c
@@ -1,1 +1,6 @@
- 
+#include<stdio.h>
+imt main()
+{
+  printf("my name is RAJ \n");
+  return 0;
+} 
---
+++ p4original.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int a,b ,c;
+  a=11;
+  b=22;
+  c=33;
+
+  if(a>b && a>c)
+}
