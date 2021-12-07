fcf57948f3971d13a81efdd411386f4201eb75e2
Raj Shekar <94944999+rajashekarm@users.noreply.github.com>
2021-12-07 03:18:37 +0000

    debugging

--- p4original.c
+++ p4original.c
@@ -9,7 +9,7 @@ int main()
 {
   float num1, num2, num3, largest;
   printf("Enter three numbers:");
-  scanf("%f %f %f", &num1, num2, num3);
+  scanf("%f %f %f", &num1, &num2, &num3);
 
   largest = large(num1, num2,num3);
   printf("Largest number = %.2f", largest );
